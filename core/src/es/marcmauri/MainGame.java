b2dc741e8fbc31261714ce355dce5ea369a57b20
Marc from GitKraken <marcmauri92@gmail.com>
2018-06-20 16:31:00 +0200

    Limpieza de proyecto para empezar de 0 con el juego

--- core/src/es/marcmauri/MainGame.java
+++ core/src/es/marcmauri/MainGame.java
@@ -1,31 +1,7 @@
 package es.marcmauri;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class MainGame extends ApplicationAdapter {
-
-    @Override
-    public void create() {
-        Procesador p = new Procesador();
-        Gdx.input.setInputProcessor(p);
-    }
-
-    @Override
-    public void dispose() {
-        // Cuando se cierra el juego, se ejecuta
-        // Importante liberar recursos en este paso
-    }
-
-    @Override
-    public void render() {
-        // Primero es importante limpiar el buffer de la grafica
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        Gdx.gl.glClearColor(1, 0, 0.5f, 1);
-    }
+    //
 }
