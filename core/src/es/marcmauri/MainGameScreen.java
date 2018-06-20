33113ab4bf84332efdd0a29d9fdb7fd2e30ad7cd
Marc from GitKraken <marcmauri92@gmail.com>
2018-06-20 17:30:14 +0200

    add actor pinchos + dotar de movimiento teniendo en cuenta el valor delta

--- core/src/es/marcmauri/MainGameScreen.java
+++ core/src/es/marcmauri/MainGameScreen.java
@@ -3,30 +3,40 @@ package es.marcmauri;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 import es.marcmauri.actors.ActorJugador;
+import es.marcmauri.actors.ActorPinchos;
 
 public class MainGameScreen extends BaseScreen {
 
     private Stage stage;
 
     private ActorJugador jugador;
+    private ActorPinchos pinchos;
 
-    private Texture texturaJugador;
+    private Texture texturaJugador, texturaPinchos;
+
+    private TextureRegion regionPinchos;
 
     public MainGameScreen(MainGame game) {
         super(game);
         texturaJugador = new Texture("player.png");
+        texturaPinchos = new Texture("pinchos.png");
+        regionPinchos = new TextureRegion(texturaPinchos, 0, 64, 128, 64);
     }
 
     @Override
     public void show() {
         stage = new Stage();
         jugador = new ActorJugador(texturaJugador);
+        pinchos = new ActorPinchos(regionPinchos);
         stage.addActor(jugador);
+        stage.addActor(pinchos);
 
         jugador.setPosition(20, 100);
+        pinchos.setPosition(500,100);
     }
 
     @Override
---
+++ core/src/es/marcmauri/actors/ActorPinchos.java
@@ -0,0 +1,25 @@
+package es.marcmauri.actors;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+public class ActorPinchos extends Actor {
+
+    private TextureRegion pinchos;
+
+    public ActorPinchos(TextureRegion pinchos) {
+        this.pinchos = pinchos;
+    }
+
+    @Override
+    public void act(float delta) {
+        // <-- -250px/seg
+        setX(getX() - 250*delta);
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        batch.draw(pinchos, getX(), getY());
+    }
+}
