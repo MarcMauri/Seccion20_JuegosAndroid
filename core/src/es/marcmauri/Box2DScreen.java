4f37196261b3d21d25f0bbf8c548a5deafb30aa0
Marc from GitKraken <marcmauri92@gmail.com>
2018-06-21 01:06:18 +0200

    Detectando y controlando las colisiones. En este caso cuando el body del player colisiona con el body del suelo

--- core/src/es/marcmauri/Box2DScreen.java
+++ core/src/es/marcmauri/Box2DScreen.java
@@ -7,7 +7,11 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.Manifold;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
@@ -27,6 +31,8 @@ public class Box2DScreen extends BaseScreen {
 
     private Fixture playerFixture, sueloFixture, pinchoFixture;
 
+    private boolean haColisionado;
+
     @Override
     public void show() {
         world = new World(new Vector2(0, -10), true);
@@ -34,13 +40,39 @@ public class Box2DScreen extends BaseScreen {
         camera = new OrthographicCamera(7.11f, 4);
         camera.translate(0, 1);
 
+        world.setContactListener(new ContactListener() {
+            @Override
+            public void beginContact(Contact contact) {
+                Fixture fixtureA = contact.getFixtureA(), fixtureB = contact.getFixtureB();
+                if (fixtureA == playerFixture && fixtureB == sueloFixture ||
+                        fixtureA == sueloFixture && fixtureB == playerFixture) {
+                    haColisionado = true;
+                }
+            }
+
+            @Override
+            public void endContact(Contact contact) {
+
+            }
+
+            @Override
+            public void preSolve(Contact contact, Manifold oldManifold) {
+
+            }
+
+            @Override
+            public void postSolve(Contact contact, ContactImpulse impulse) {
+
+            }
+        });
+
         playerBody = world.createBody(createPlayerBodyDef());
         sueloBody = world.createBody(createSueloBodyDef());
         pinchoBody = world.createBody(createPinchoBodyDef(0.5f));
 
         PolygonShape playerShape = new PolygonShape();
         playerShape.setAsBox(0.5f, 0.5f);
-        playerFixture = playerBody.createFixture(playerShape, 1);
+        playerFixture = playerBody.createFixture(playerShape, 3);
         playerShape.dispose();
 
         PolygonShape sueloShape = new PolygonShape();
@@ -101,8 +133,10 @@ public class Box2DScreen extends BaseScreen {
     public void render(float delta) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        if (Gdx.input.justTouched())
+        if (Gdx.input.justTouched() || haColisionado) {
+            haColisionado = false;
             saltar();
+        }
 
         world.step(delta, 6, 2);
 
@@ -112,6 +146,6 @@ public class Box2DScreen extends BaseScreen {
 
     private void saltar() {
         Vector2 position = playerBody.getPosition();
-        playerBody.applyLinearImpulse(0, 6, position.x, position.y, true);
+        playerBody.applyLinearImpulse(0, 20, position.x, position.y, true);
     }
 }
