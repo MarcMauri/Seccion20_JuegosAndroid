b032c114a4e5b2f12bc7a3f520a01861f43e7c1a
Marc from GitKraken <marcmauri92@gmail.com>
2018-06-21 16:57:11 +0200

    Nuevos menus y modificaciones directamente desde import static es.marcmauri.Constants.PIXELS_IN_METER;  porque no hay dios quien lo pille

--- core/src/es/marcmauri/MainGame.java
+++ core/src/es/marcmauri/MainGame.java
@@ -8,25 +8,50 @@ import com.badlogic.gdx.graphics.Texture;
 
 public class MainGame extends Game {
 
+    /** This is the asset manager we use to centralize the assets. */
     private AssetManager manager;
 
-    public AssetManager getManager() {
-        return manager;
-    }
+    /**
+     * These are the screens that we use in this game. I invite you to use a better system than
+     * just public variables. For instance, you could create an ArrayList or maybe use some
+     * structure such as a map where you can associate a number or a string to a screen.
+     */
+    public BaseScreen loadingScreen, menuScreen, gameScreen, gameOverScreen;
 
     @Override
     public void create() {
+        // Initialize the asset manager. We add every aset to the manager so that it can be loaded
+        // inside the LoadingScreen screen. Remember to put the name of the asset in the first
+        // argument, then the type of the asset in the second argument.
         manager = new AssetManager();
         manager.load("floor.png", Texture.class);
+        manager.load("gameover.png", Texture.class);
         manager.load("overfloor.png", Texture.class);
+        manager.load("logo.png", Texture.class);
         manager.load("spike.png", Texture.class);
         manager.load("player.png", Texture.class);
-        manager.load("gameover.png", Texture.class);
         manager.load("audio/die.ogg", Sound.class);
         manager.load("audio/jump.ogg", Sound.class);
         manager.load("audio/song.ogg", Music.class);
-        manager.finishLoading();
 
-        setScreen(new GameOverScreen(this));
+        // Enter the loading screen to load the assets.
+        loadingScreen = new LoadingScreen(this);
+        setScreen(loadingScreen);
+    }
+
+    /**
+     * This method is invoked by LoadingScreen when all the assets are loaded. Use this method
+     * as a second-step loader. You can load the rest of the screens here and jump to the main
+     * screen now that everything is loaded.
+     */
+    public void finishLoading() {
+        menuScreen = new MenuScreen(this);
+        gameScreen = new GameScreen(this);
+        gameOverScreen = new GameOverScreen(this);
+        setScreen(menuScreen);
+    }
+
+    public AssetManager getManager() {
+        return manager;
     }
 }
--- core/src/es/marcmauri/entities/PlayerEntity.java
+++ core/src/es/marcmauri/entities/PlayerEntity.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of Jump Don't Die
+ * Copyright (C) 2015 Dani Rodríguez <danirod@outlook.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package es.marcmauri.entities;
 
 import com.badlogic.gdx.Gdx;
@@ -13,66 +31,118 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 
 import es.marcmauri.Constants;
 
-import static es.marcmauri.Constants.PIXELS_IN_METER;
 
 public class PlayerEntity extends Actor {
 
+    /**
+     * The player texture.
+     */
     private Texture texture;
+
+    /**
+     * The world instance this player is in.
+     */
     private World world;
+
+    /**
+     * The body for this player.
+     */
     private Body body;
+
+    /**
+     * The fixture for this player.
+     */
     private Fixture fixture;
-    private boolean alive = true, jumping = false, mustJump = false;
+
+    /**
+     * Is the player alive? If he touches a spike, is not alive. The player will only move and
+     * jump if it's alive. Otherwise it is said that the user has lost and the game is over.
+     */
+    private boolean alive = true;
+
+    /**
+     * Is the player jumping? If the player is jumping, then it is not possible to jump again
+     * because the user cannot double jump. The flag has to be set when starting a jump and be
+     * unset when touching the floor again.
+     */
+    private boolean jumping = false;
+
+    /**
+     * Does the player have to jump? This flag is used when the player touches the floor and the
+     * user is still touching the screen, to make a double jump. Remember that we cannot add
+     * a force inside a ContactListener. We have to use this flag to remember that the player
+     * had to jump after the collision.
+     */
+    private boolean mustJump = false;
 
     public PlayerEntity(World world, Texture texture, Vector2 position) {
         this.world = world;
         this.texture = texture;
 
-        BodyDef def = new BodyDef();
-        def.position.set(position);
-        def.type = BodyDef.BodyType.DynamicBody;
-        body = world.createBody(def);
+        // Create the player body.
+        BodyDef def = new BodyDef();                // (1) Create the body definition.
+        def.position.set(position);                 // (2) Put the body in the initial position.
+        def.type = BodyDef.BodyType.DynamicBody;    // (3) Remember to make it dynamic.
+        body = world.createBody(def);               // (4) Now create the body.
 
-        PolygonShape box = new PolygonShape();
-        box.setAsBox(0.5f, 0.5f);
-        fixture = body.createFixture(box, 3);
-        fixture.setUserData("player");
-        box.dispose();
+        // Give it some shape.
+        PolygonShape box = new PolygonShape();      // (1) Create the shape.
+        box.setAsBox(0.5f, 0.5f);                   // (2) 1x1 meter box.
+        fixture = body.createFixture(box, 3);       // (3) Create the fixture.
+        fixture.setUserData("player");              // (4) Set the user data.
+        box.dispose();                              // (5) Destroy the shape.
 
-        // La caja mide 1m x 1m. Traducido son 90px x 90 px (1m x 90px)
-        setSize(PIXELS_IN_METER, PIXELS_IN_METER);
+        // Set the size to a value that is big enough to be rendered on the screen.
+        setSize(Constants.PIXELS_IN_METER, Constants.PIXELS_IN_METER);
     }
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
-        setPosition(
-                (body.getPosition().x - 0.5f) * PIXELS_IN_METER,
-                (body.getPosition().y - 0.5f) * PIXELS_IN_METER
-        );
+        // Always update the position of the actor when you are going to draw it, so that the
+        // position of the actor on the screen is as accurate as possible to the current position
+        // of the Box2D body.
+        setPosition((body.getPosition().x - 0.5f) * Constants.PIXELS_IN_METER,
+                (body.getPosition().y - 0.5f) * Constants.PIXELS_IN_METER);
         batch.draw(texture, getX(), getY(), getWidth(), getHeight());
     }
 
     @Override
     public void act(float delta) {
-        // Iniciar un salto si hemos tocado la pantalla
+        // Jump when you touch the screen.
+        if (Gdx.input.justTouched()) {
+            jump();
+        }
+
+        // Jump if we were required to jump during a collision.
         if (mustJump) {
             mustJump = false;
             jump();
         }
 
-        // Hacer avanzar el jugador si esta vivo
+        // If the player is alive, change the speed so that it moves.
         if (alive) {
+            // Only change X speed. Do not change Y speed because if the player is jumping,
+            // this speed has to be managed by the forces applied to the player. If we modify
+            // Y speed, jumps can get very very weir.d
             float speedY = body.getLinearVelocity().y;
             body.setLinearVelocity(Constants.PLAYER_SPEED, speedY);
         }
 
+        // If the player is jumping, apply some opposite force so that the player falls faster.
         if (jumping) {
             body.applyForceToCenter(0, -Constants.IMPULSE_JUMP * 1.15f, true);
         }
     }
 
-    private void jump() {
+    public void jump() {
+        // The player must not be already jumping and be alive to jump.
         if (!jumping && alive) {
             jumping = true;
+
+            // Apply an impulse to the player. This will make change the velocity almost
+            // at the moment unlike using forces, which gradually changes the force used
+            // during the jump. We get the position becase we have to apply the impulse
+            // at the center of mass of the body.
             Vector2 position = body.getPosition();
             body.applyLinearImpulse(0, Constants.IMPULSE_JUMP, position.x, position.y, true);
         }
@@ -83,6 +153,8 @@ public class PlayerEntity extends Actor {
         world.destroyBody(body);
     }
 
+    // Getter and setter festival below here.
+
     public boolean isAlive() {
         return alive;
     }
@@ -91,16 +163,8 @@ public class PlayerEntity extends Actor {
         this.alive = alive;
     }
 
-    public boolean isJumping() {
-        return jumping;
-    }
-
     public void setJumping(boolean jumping) {
         this.jumping = jumping;
-    }
-
-    public boolean isMustJump() {
-        return mustJump;
     }
 
     public void setMustJump(boolean mustJump) {
